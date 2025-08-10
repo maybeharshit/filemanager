df997d984b94c939065775091af30aed6a1c0feb
maybeharshit <nowyouseeme2203@gmail.com>
2025-08-11 02:45:18 +0530

    Initial

---
+++ filemanager.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
+#include<string.h>
+
+void main() {
+	
+	//initialize virtual desktop
+	//set "home" as default directory
+	
+	return;
+}
+
